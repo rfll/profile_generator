a86a63655dc8a1514dacf0e573a4753b871a7123
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-03 05:50:39 +0000

    Testing nested questions

--- survey.js
+++ survey.js
@@ -13,16 +13,25 @@ const answers = {};
 rl.question('What is your name? Nicknames are also acceptable. ', (name) => { 
   answers.name = name;
 
-  rl.question('What is your age ', (age) => { 
-    answers.age = age;
+  rl.question('What is an activity you like doing? ', (activity) => { 
+    answers.activity = activity;
 
-    rl.close();
+    rl.question('What do you like to listen to while doing that? ', (audio) => { 
+      answers.audio = audio;
 
-    console.log(`${name} and ${age}`)
+      rl.question('What meal is your favourite? (e.g. dinner, brunch etc.) ', (meal) => { 
+        answers.meal = meal;
+
+        rl.close();
+
+        console.log(`${name} ${activity} ${audio} ${meal}`);
+    
+      });
+  
+    });
 
   });
 
 
 });
-
 
