1a66569fde33c0eca6501080fe1cd93995c92bb5
Ryan Fortin <ryanwfortin@gmail.com>
2022-08-03 06:18:34 +0000

    Added .toLowerCase to object values in case answers are provided with a capital letter.

--- survey.js
+++ survey.js
@@ -14,24 +14,31 @@ rl.question('What is your name? Nicknames are also acceptable. ', (name) => {
   answers.name = name;
 
   rl.question('What is an activity you like doing? ', (activity) => { 
-    answers.activity = activity;
+    answers.activity = activity.toLowerCase();
 
     rl.question('What do you like to listen to while doing that? ', (audio) => { 
-      answers.audio = audio;
+      answers.audio = audio.toLowerCase();
 
       rl.question('What meal is your favourite? (e.g. dinner, brunch etc.) ', (meal) => { 
-        answers.meal = meal;
+        answers.meal = meal.toLowerCase();
 
-        rl.close();
+        rl.question('What is your favourite thing to eat for that meal? ', (food) => { 
+          answers.food = food.toLowerCase();
 
-        console.log(`${name} ${activity} ${audio} ${meal}`);
-    
+          rl.question('Which sport is your absolute favourite? ', (sport) => { 
+            answers.sport = sport.toLowerCase();
+
+            rl.question('What is your superpower? In a few words tell us what you are amazing at! ', (superpower) => { 
+              answers.superpower = superpower.toLowerCase();
+      
+              rl.close();
+              console.log(`${answers.name} loves listening to ${answers.audio} while ${answers.activity}, devouring ${answers.food} for ${answers.meal}, loves ${answers.sport} and is amazing at ${answers.superpower}!`);
+          
+            });
+          });
+        });
       });
-  
     });
-
   });
-
-
 });
 
